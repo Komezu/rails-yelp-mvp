87dd5f9f4941ac00ab79c4faf6af87d6caab2874
Jin Qi <jenny_qiqu@hotmail.com>
2022-11-09 15:29:28 -0500

    improved app by making review form in-page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root to: 'restaurants#index'
   resources :restaurants, only: %i[index show new create] do
-    resources :reviews, only: %i[new create]
+    # resources :reviews, only: %i[new create]
+    resources :reviews, only: :create
   end
 end
